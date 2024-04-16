98716ecd71e0010e847b638ed189a738ad5e18e3
ilman01 <ilmanzidni2006@gmail.com>
2024-04-16 18:03:44 +0700

    Added type annotations

--- ycls/ycls.py
+++ ycls/ycls.py
@@ -1,15 +1,15 @@
 import r128gain
 import typer
 
-def calculate_loudness_lufs(input_file):
+def calculate_loudness_lufs(input_file: str) -> float:
     '''Calculates the loudness level of the input audio (or video) file in LUFS (Loudness Units Full Scale) using the r128gain library.'''
     return r128gain.get_r128_loudness([input_file])[0]
 
-def calculate_youtube_content_loudness(input_file):
+def calculate_youtube_content_loudness(input_file: str) -> float:
     '''Calculates the adjusted loudness suitable for YouTube content based on the input audio (or video) file. It adds 14 dB to the loudness level calculated by calculate_loudness_lufs.'''
     return round(calculate_loudness_lufs(input_file) + 14, 1)
 
-def calculate_peak_dbfs(input_file):
+def calculate_peak_dbfs(input_file: str) -> float:
     '''Calculates the peak loudness in dBFS (decibels relative to full scale) of the input audio (or video) file using the r128gain library.'''
     return round(r128gain.scale_to_gain(r128gain.get_r128_loudness([input_file])[1]), 1)
 
